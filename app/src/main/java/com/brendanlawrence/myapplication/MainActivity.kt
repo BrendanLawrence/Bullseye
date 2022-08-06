88f3ad5ca9d002d03639936388471553f6d97e4e
Brendan Lawrence <brendan.c.lawrence@gmail.com>
2022-08-06 16:16:34 +1000

    added rounds, restart game

--- app/src/main/java/com/brendanlawrence/myapplication/MainActivity.kt
+++ app/src/main/java/com/brendanlawrence/myapplication/MainActivity.kt
@@ -11,7 +11,9 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var sliderValue = 0
-    private var targetValue = Random.nextInt(1, 100);
+    private var targetValue = newTargetValue()
+    private var totalScore = 0
+    private var round = 1
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,11 +25,17 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root // Using val instead of var as we will not be changing this 'view' object once created.
         setContentView(view)
 
-        binding.targetTextView.text = targetValue.toString()
+        startNewGame()
 
         binding.hitMeButton.setOnClickListener {
-            Log.i("Button Click Event", "You clicked the Hit Me Button")
+//            Log.i("Button Click Event", "You clicked the Hit Me Button")
             showResult()
+            totalScore += pointsForCurrentRound()
+            binding.gameScoreTextView?.text = totalScore.toString()
+        }
+
+        binding.startOverButton?.setOnClickListener {
+            startNewGame()
         }
 
         binding.targetSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -41,9 +49,19 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun differenceAmount() = abs(sliderValue - targetValue)
+
+    private fun newTargetValue () = Random.nextInt(1, 100);
+
     private fun pointsForCurrentRound(): Int {
         val maxScore: Int = 100
-        val difference = abs(sliderValue - targetValue)
+        val difference = differenceAmount()
+        var points = maxScore - difference
+
+        if (difference == 0) {
+            points += 100
+        } else if (difference == 1)
+            points += 50
 
 //        var difference: Int = if (sliderValue > targetValue) {
 //            sliderValue - targetValue
@@ -60,11 +78,23 @@ class MainActivity : AppCompatActivity() {
 //
 //        return maxScore - difference;
 
-        return maxScore - difference
+        return points
+    }
+
+    private fun startNewGame() {
+        totalScore = 0
+        round = 0
+        sliderValue = 50
+        targetValue = newTargetValue()
+
+        binding.gameScoreTextView?.text = totalScore.toString()
+        binding.gameRoundTextView?.text = round.toString()
+        binding.targetTextView.text = targetValue.toString()
+        binding.targetSeekBar.progress = sliderValue
     }
 
     private fun showResult() {
-        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogTitle = alertTitle()
         val dialogMessage = getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
 //        val dialogMessage = "The slider's value is $sliderValue"
 
@@ -73,9 +103,35 @@ class MainActivity : AppCompatActivity() {
         builder.setTitle(dialogTitle)
         builder.setMessage(dialogMessage)
         builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ ->
+            round++
+            binding.gameRoundTextView?.text = round.toString()
+
+            targetValue = newTargetValue()
+            binding.targetTextView.text = targetValue.toString()
             dialog.dismiss()
         }
 
         builder.create().show()
+    }
+
+    private fun alertTitle(): String {
+        val difference = differenceAmount()
+
+        val title: String = when {
+            difference == 0 -> {
+                getString(R.string.alert_title_1)
+            }
+            difference < 5 -> {
+                getString(R.string.alert_title_2)
+            }
+            difference <= 10 -> {
+                getString(R.string.alert_title_3)
+            }
+            else -> {
+                getString(R.string.alert_title_4)
+            }
+        }
+
+        return title;
     }
 }
